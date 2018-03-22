231ae8512cea634b09de7968208dc38ac9d174da
FilitovDemo <filitov@twcat.net>
2018-03-22 14:47:23 +0800

    確定參數沒問題

--- 0322/t1.php
+++ 0322/t1.php
@@ -1,5 +1,6 @@
 <?php
-if( !isset($_GET['aa']) || !isset($_GET['bb'])
+if( !isset($_GET['aa']) 
+	|| !isset($_GET['bb'])
 	|| !is_numeric($_GET['aa']) 
 	|| !is_numeric($_GET['bb']) ){
 	echo "參數不正確";
@@ -16,11 +17,7 @@ if( !is_numeric($_GET['aa']) || !is_numeric($_GET['bb']) ){
 	exit;	
 }
 */
-
-
 $x = $_GET['aa'];
 $y = $_GET['bb'];
-
 $z = $x+$y;
-
 echo $z;
